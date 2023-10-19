a65caa7cc3db5492b9ad25d0af52cc32c9662b8e
Dmitri Lebedev <devnull@localhost>
2023-10-19 18:57:44 +0600

    finalized tests in table.rs

--- src/lib.rs
+++ src/lib.rs
@@ -56,21 +56,24 @@ impl<T: TableRecord> Table<T> {
 		Ok(())
 	}
 
-	fn upsert(&mut self, key: T::Key, val: T) {
-		let new_cats: HashSet<T::Category> = val.categories().into_iter().collect();
+	/// Finds the object by old key, updates it. The key in the table is not updated.
+	// TODO: make it updated
+	fn upsert(&mut self, old_key: T::Key, new_val: T) {
+		let new_cats = vec2hashset(new_val.categories());
 		// do update
-		if self.contains_key(&key) {
-			self.update(key, &|old_val| *old_val = val.clone());
+		if self.contains_key(&old_key) {
+			self.update(old_key, &|old_val| *old_val = new_val.clone());
 		} else {
-			self.insert(val).unwrap();
+			self.insert(new_val).unwrap();
 		}
 	}
 
+	// TODO: make it update the key change
 	fn update(&mut self, key: T::Key, cb: &impl Fn(&mut T)) -> bool {
 		let Some(mut val) = self.data.get_mut(&key) else { return false; };
-		let old_cats: HashSet<T::Category> = val.categories().into_iter().collect();
+		let old_cats = vec2hashset(val.categories());
 		cb(&mut val);
-		let new_cats = val.categories().into_iter().collect();
+		let new_cats = vec2hashset(val.categories());
 
 		for c in old_cats.difference(&new_cats) {
 			self.index.entry(c.clone()).and_modify(|v| v.retain(|k| *k != key));
@@ -110,11 +113,12 @@ impl<T: TableRecord> Table<T> {
 	}
 
 	fn find_many(&self, cats: &[T::Category]) -> Vec<&T> { // TODO: replace with iterator struct
-		cats.iter()
+		let keys: HashSet<&T::Key> = cats.iter()
 			.filter_map(|c| self.index.get(c))
-			.flatten() // Vec<T>s into T-s
-			.filter_map(|k| self.data.get(k)) //
-			.collect()
+			.flatten().collect();
+
+		 // Vec<T>s into T-s
+		keys.iter().filter_map(|k| self.data.get(k)).collect()
 	}
 
 	fn iter(&self) -> impl Iterator<Item = (&T::Key, &T)> {
@@ -134,6 +138,9 @@ impl<T: TableRecord> Table<T> {
 	}
 }
 
+fn vec2hashset<T: Hash + Eq>(data: Vec<T>) -> HashSet<T> {
+	data.into_iter().collect()
+}
 
 #[cfg(test)]
 pub mod multimap_tests {
@@ -172,19 +179,16 @@ pub mod multimap_tests {
 		}
 	}
 
-	#[test]
-	fn test_index_table() {
-		let mut it: Table<Book> = Table::new();
+	fn books_fixture() -> Vec<Book> {
 		let s2 = ScienceId(2);
 		let s3 = ScienceId(3);
 		let s4 = ScienceId(4);
-		let s5 = ScienceId(5);
 
 		let a0 = AuthorId(10);
 		let a1 = AuthorId(11);
 		let a2 = AuthorId(12);
 
-		let books = vec![
+		vec![
 			Book { id: BookId(1), title: "Book №1".into(), science: s2, author: a0 },
 			Book { id: BookId(2), title: "Book №2".into(), science: s2, author: a1 },
 			Book { id: BookId(3), title: "Book №3".into(), science: s2, author: a2 },
@@ -194,39 +198,150 @@ pub mod multimap_tests {
 			Book { id: BookId(6), title: "Book №6".into(), science: s3, author: a2 },
 
 			Book { id: BookId(7), title: "Book №7".into(), science: s4, author: AuthorId(13) }, // alone in both categories
-		];
+		]
+	}
+
+	fn table_fixture() -> Table<Book> {
+		let mut it: Table<Book> = Table::new();
+		let books = books_fixture();
 
 		for b  in books.clone().into_iter() {
 			it.insert(b).unwrap();
 		}
 
+		it
+	}
+
+	#[test]
+	fn test_contains_key() {
+		let books = books_fixture();
+		let it = table_fixture();
 		for b in books.iter() {
 			assert!(it.contains_key(&b.key()));
 		}
 
 		assert!(!it.contains_key(&BookId(1000)));
+	}
+
+	#[test]
+	fn test_categories() {
+		let s2 = ScienceId(2);
+		let s3 = ScienceId(3);
+		let s4 = ScienceId(4);
+		let s5 = ScienceId(5);
+		let books = books_fixture();
+		let it = table_fixture();
+
 		assert_eq!(it.len(), 7);
 		for science in &[s2, s3, s4] {
 			assert!(it.contains_cat(&BookCategory::Science(science.clone())));
 		}
 		assert!(!it.contains_cat(&BookCategory::Science(s5)));
 
-		let expected_values: HashSet<Book> = books.clone().into_iter().collect();
-		let real_values: HashSet<Book> = it.values().map(|b| b.clone()).collect();
+		let expected_values = vec2hashset(books.clone());
+		let real_values = it.values().map(|b| b.clone()).collect();
 		assert_eq!(expected_values, real_values);
+	}
 
+	#[test]
+	fn test_contains_val() {
+		let it = table_fixture();
+		let books = books_fixture();
 		for b in books.iter() {
 			assert!(it.contains_val(b));
 		}
+	}
 
-		// upsert a book
+	#[test]
+	fn test_insert() {
+		let mut it = table_fixture();
+		let books = books_fixture();
+		let res = it.insert(books[0].clone()).is_err();
+		assert!(res);
+
+		println!("{}", res);
+		println!("{:?}", res);
+	}
+
+	#[test]
+	fn test_upsert() {
+		let mut it = table_fixture();
+		let books = books_fixture();
+		let s3 = ScienceId(3);
+		let a0 = AuthorId(10);
+		let a1 = AuthorId(11);
+
+		// upsert a book (update existing one)
 		// write book5 into book2
+		let old_len = it.len();
 		let b2 = books[1].clone();
 		// find books by book 2 author (a1)
 		let prev_author_books = it.find(&BookCategory::Author(b2.author)).len();
-		it.upsert(BookId(2), Book { id: BookId(5), title: "Book №5".into(), science: s3, author: a0 });
+		// TODO: what if we try to change the key? Should check for collisions?
+		it.upsert(BookId(2), Book { id: BookId(2), title: "Book №5".into(), science: s3, author: a0 });
 		// less 1 book by author (a1)
 		let curr_author_books = it.find(&BookCategory::Author(b2.author)).len();
 		assert_eq!(prev_author_books - 1, curr_author_books);
+		assert_eq!(it.len(), old_len);
+
+
+		// upsert a new book
+		// adds new category
+		it.upsert(BookId(10), Book { id: BookId(10), title: "Book 10".into(), science: ScienceId(100), author: a1});
+		// update book with no new category
+		it.upsert(BookId(10), Book { id: BookId(10), title: "Book №10".into(), science: ScienceId(101), author: a1});
+		assert_eq!(it.len(), old_len + 1);
 	}
+
+	#[test]
+	fn test_update_by_cat() {
+		let a2 = AuthorId(2);
+		let mut it = table_fixture();
+		let books = books_fixture();
+		let b3 = books[2].clone();
+
+		// update category: set category 2 to 3 (also checking for collision of editing/reading categories and writing)
+		it.update_by_cat(BookCategory::Science(ScienceId(3)), |b| b.science = ScienceId(4));
+
+		let old_len = it.len();
+		it.remove(&BookId(1));
+		assert_eq!(old_len - 1, it.len());
+
+		// update non existent book
+		assert!(!it.update(BookId(123456), &|b| b.author = a2)); // must return false
+
+		assert_eq!(it.get(&BookId(3)), Some(&b3));
+		assert_eq!(it.get(&BookId(654321)), None);
+
+	}
+
+	#[test]
+	fn test_iter() {
+		let it = table_fixture();
+		let real: HashSet<_> = it.iter().collect();
+		let bf = books_fixture();
+		let expected: HashSet<_> = bf.iter().map(|b| (&b.id, b)).collect();
+		assert_eq!(expected, real);
+
+		let real: HashSet<_> = it.values().collect();
+		let expected: HashSet<_> = bf.iter().collect();
+		assert_eq!(real, expected);
+
+		let real: HashSet<_> = it.iter_keys().collect();
+		let expected: HashSet<_> = bf.iter().map(|b| &b.id).collect();
+		assert_eq!(real, expected);
+
+		let real: HashSet<_> = it.iter_cats().map(|c| c.clone()).collect();
+		let expected: HashSet<_> = bf.iter().map(|b| vec![BookCategory::Science(b.science), BookCategory::Author(b.author)]).flatten().collect();
+		assert_eq!(real, expected);
+	}
+
+	#[test]
+	fn find_many() {
+		let it = table_fixture();
+		let real: HashSet<_> = it.find_many(&[BookCategory::Science(ScienceId(2)), BookCategory::Author(AuthorId(10))]).iter().map(|b| b.id.0).collect();
+		let expected: HashSet<usize> = collection!(1, 2, 3, 4);
+		assert_eq!(real, expected);
+	}
+
 }
